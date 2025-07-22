0931eab88e5d198dbf598855c6aeeab24b7bb665
Ashutosh Choubey <ashutoshchoubey16@gmail.com>
2025-07-23 00:52:51 +0530

    20 July - Rest Assured 1st class

---
+++ src/test/java/org/example/ex_01_RA_Concepts/RestHello.java
@@ -0,0 +1,13 @@
+package org.example.ex_01_RA_Concepts;
+
+public class RestHello {
+    public static void main(String[] args) {
+
+        RestAssured.given()
+                .baseUri("https://api.postalpincode.in/pincode/560036")
+                .when()
+                .get()
+                .then()
+                .log().all().statusCode(200);
+    }
+}
