ea053c97e0022d631bdd95b9158542d070eddcff
Ashutosh Choubey <ashutoshchoubey16@gmail.com>
2025-08-04 20:34:40 +0530

    2nd & 3rd Aug

---
+++ src/test/java/org/example/ex_05_TestNGExamples/APITesting014.java
@@ -0,0 +1,14 @@
+package org.example.ex_05_TestNGExamples;
+
+import org.testng.annotations.Test;
+
+public class APITesting014 {
+    public void test_post_request(){   // this is not a correct test method
+
+    }
+
+    @Test
+    public void test_post_request2(){   // this is a correct test method
+
+    }
+}
